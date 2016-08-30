608068ec0e352d78c397099851e1aef04dffc019
Aji Lantang Mardika & Fadhli Ramadhani Arislan <pair+ajilantang.fadhlirams@hacktiv8.com>
2016-08-30 19:44:15 +0700

    dictionary

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,7 +1,34 @@
-
+var arr = [];
+var words = []
+var hasil = [];
 function dictionary_sort(arr) {
-  // Your code here to sort the array
+
+  return arr;
 }
 
+  // Your code here to sort the array
 // ...your code here to initialize the program and take user input
+var words = new Array();
+var tampung = prompt("Test a word");
+words.push(tampung);
 
+while (tampung!==""){
+  tampung = prompt("Test a word again : ")
+  words.push(tampung);
+}
+
+console.log(words.sort());
+hasil = words.sort();
+
+
+console.log("Congratulations! Your dictionary has "+(hasil.length)+" words");
+for(var i = 1 ; i < hasil.length ; i++)
+{
+  console.log(hasil[i]);
+}
+
+
+
+
+
+// ...your code here to initialize the program and take user input
