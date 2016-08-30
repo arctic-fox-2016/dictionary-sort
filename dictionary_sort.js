f1ec96c48944fbc074f0516f758f33d0719d1f5f
Septhianto Diga & Toni <pair+digachandra.tonychen09@hacktiv8.com>
2016-08-30 17:01:57 +0700

    Update code

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,51 +1,23 @@
 let dictionary_array = [];
-let sort_array = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A','B','C','D','E','F','G','H','I','J','K','L','M',
-'N','O','P','Q','R','S',
-'T','U','V','W','X','Y','Z'];
 
 function dictionary_sort(arr){
-  var array_value = [];
-  for(let idx = 0; idx < arr.length; idx++){
-    array_value[idx] = 0;
-    for(let idy = 0; idy < arr[idx].length; idy++){
-      for(let idz = 0; idz < sort_array.length; idz++){
-        if(arr[idx].charAt(idy).toLowerCase() == sort_array[idz].toLowerCase()){
-          array_value[idx] += idz;
-        }
-      }
-    }
-    //array_value[idx] *= Math.pow(10, arr[idx].length);
-    array_value[idx] += arr[idx].length;
-    //console.log(array_value[idx] + " - " + test_array[idx]);
-  }
-
   for(let idx = 0; idx < arr.length; idx++){
     for(let idy = idx+1; idy < arr.length; idy++){
-      if(array_value[idy] < array_value[idx]){
-        var temp_value = array_value[idx];
-        array_value[idx] = array_value[idy];
-        array_value[idy] = temp_value;
-
-        var temp_text = arr[idx];
+      if(arr[idy] < arr[idx]){
+        var temp_value = arr[idx];
         arr[idx] = arr[idy];
-        arr[idy] = temp_text;
+        arr[idy] = temp_value;
         idx = 0;
       }
     }
   }
 
-  //console.log("==================");
-
   for(let idx = 0; idx < arr.length; idx++){
-    //console.log(array_value[idx] + " - " + test_array[idx]);
     console.log(arr[idx]);
   }
 
   rl.close();
 }
-
-//let test_array = ["Z", "Doe", "Ipsum", "Lorem"]; BUGGY
-//dictionary_sort(test_array); BUGGY
 
 // ...your code here to initialize the program and take user input
 console.log("$ node dictionary_sort.js");
